1ea95f24c15d421e061988eaded6c820b9859361
Andrew Migal <andrey.migal@gmail.com>
2012-10-15 11:21:23 +0300

    04_p_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,7 +1,5 @@
-
 def translate(st)
   st.split(' ').map do |s|
-	s.sub(/^(qu|[^aioue]{1,})(\w+)$/,'\\2\\1') + 'ay'
-  end
-  .join(' ')
+    s.sub(/^(qu|[^aioue]{1,})(\w+)$/, '\\2\\1') + 'ay'
+  end.join(' ')
 end
