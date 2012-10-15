3fcc856593beaf6daef0cddc0ef848471cd99038
Andrew Migal <andrey.migal@gmail.com>
2012-10-15 11:46:49 +0300

    09_book

--- 09_book_titles/book.rb
+++ 09_book_titles/book.rb
@@ -1,26 +1,14 @@
 class Book
-	#
-	
-	def title
-		@title
-	end
+  attr_accessor :title
 
-	def title=(new_title)
-		@title = new_title
-	end
-	
-	def title
-		bad_words = %w(and over the in of a an)
-		c = []
-		@title.split(" ").each do|w| 
-		 if bad_words.include?(w) then
-		 	c.push(w)
-		 else
-			c.push(w.capitalize)
-		 end
-		end
-		c[0].capitalize!
-		c.join(" ")
-		
-	end
+  def title
+    bad_words = %w(and over the in of a an)
+    c         = []
+    @title.split(' ').each do |w|
+      bad_words.include?(w) ? c.push(w) : c.push(w.capitalize)
+    end
+    c[0].capitalize!
+    c.join(' ')
+  end
+
 end
