1a888f731c1f8d7d734bfceb4fcaaff845c4cf55
Andrew Migal <andrey.migal@gmail.com>
2012-10-15 11:49:02 +0300

    10_timer

--- 10_timer/timer.rb
+++ 10_timer/timer.rb
@@ -1,18 +1,18 @@
 class Timer
-	def initialize
-		@seconds = 0
-	end
+  def initialize
+    @seconds = 0
+  end
 
-	attr_accessor :seconds
+  attr_accessor :seconds
 
-	def time_string
-		r = [0, 0, 0]
-        s = @seconds
-		2.downto(0) do |i|
-			r[i] += s % 60
-			s = s / 60
-		end
-		r.map{|el| sprintf("%02d",el)}.join(":")
-	end
-	
+  def time_string
+    r = [0, 0, 0]
+    s = @seconds
+    2.downto(0) do |i|
+      r[i] += s % 60
+      s = s / 60
+    end
+    r.map { |el| sprintf('%02d', el) }.join(':')
+  end
+
 end
