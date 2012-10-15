4df63a5fd709af46d8d260ed4b76d38a7037b148
Andrew Migal <andrey.migal@gmail.com>
2012-10-15 12:11:19 +0300

    12_calculator

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -1,69 +1,70 @@
 class RPNCalculator
-	attr_accessor :value
-	def initialize
-		@calculator = []
-		@value = []
-	end
-	
-	def push(v)
-		@calculator.push(v)
-	end
-	
-	def plus
-		raise "calculator is empty" if @calculator.empty?
-		@value = @calculator.pop(2).inject(0, &:+)
-		@calculator.push(@value)
-		
+  attr_accessor :value
 
-	end
+  def initialize
+    @calculator = []
+    @value = []
+  end
 
-	def minus
-		raise "calculator is empty" if @calculator.empty?
-		@value = @calculator.pop(2)
-		@value = @value.last - @value.first
-		@calculator.push(@value)
+  def push(v)
+    @calculator.push(v)
+  end
 
-	end
+  def plus
+    raise 'calculator is empty' if @calculator.empty?
+    @value = @calculator.pop(2).inject(0, &:+)
+    @calculator.push(@value)
 
-	def divide
-		raise "calculator is empty" if @calculator.empty?
-		@value = @calculator.pop(2)
-		@value = @value.last.to_f / @value.first.to_f
-		@calculator.push(@value)
-	end
 
-	def times
-		raise "calculator is empty" if @calculator.empty?
-		@value = @calculator.pop(2).inject(1, &:*)
-		@calculator.push(@value)
-	end
+  end
 
-	def tokens(st)
-		st.split.map do |s|
-			if s =~ /\d+/ 
-				then s.to_i 
-			else 
-				s.to_sym
-			end
-		end
-	end
+  def minus
+    raise 'calculator is empty' if @calculator.empty?
+    @value = @calculator.pop(2)
+    @value = @value.last - @value.first
+    @calculator.push(@value)
 
-	def evaluate(st)
-		st.split.map do |s|
-			
-			case s
-			when /\d+/
-				push(s.to_i)
-			when "+" 
-				plus
-			when "-"
-				minus
-			when "*" 
-				times
-			when "/"
-				divide
-			end
-		end	
-		@calculator.pop
-	end
+  end
+
+  def divide
+    raise 'calculator is empty' if @calculator.empty?
+    @value = @calculator.pop(2)
+    @value = @value.last.to_f / @value.first.to_f
+    @calculator.push(@value)
+  end
+
+  def times
+    raise 'calculator is empty' if @calculator.empty?
+    @value = @calculator.pop(2).inject(1, &:*)
+    @calculator.push(@value)
+  end
+
+  def tokens(st)
+    st.split.map do |s|
+      if s =~ /\d+/
+      then
+        s.to_i
+      else
+        s.to_sym
+      end
+    end
+  end
+
+  def evaluate(st)
+    st.split.map do |s|
+      case s
+      when /\d+/
+        push(s.to_i)
+      when '+'
+        plus
+      when '-'
+        minus
+      when '*'
+        times
+      when '/'
+        divide
+      end
+    end
+    @calculator.pop
+  end
 end
