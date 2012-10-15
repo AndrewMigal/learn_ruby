dcb33f3486d3e78f42ade576bbaadd7e515d2e96
Andrew Migal <andrey.migal@gmail.com>
2012-10-15 11:12:32 +0300

    03_simon

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,37 +1,29 @@
 def echo(x)
- x
+  x
 end
 
 def shout(x)
-	x.upcase
+  x.upcase
 end
 
-
-
- def repeat(str, count=2)
- 	("#{str} "*count).chop
+def repeat(str, count=2)
+  ("#{str} "*count).chop
 end
 
-
-
-def start_of_word(w,n)
-	w[0...n]
+def start_of_word(w, n)
+  w[0...n]
 end
 
 def first_word(s)
-	s.split(" ")[0]
+  s.split(' ')[0]
 end
 
 def titleize(s)
-	bad_words = %w(and over the in of)
-	c = []
-	s.split(" ").each do |w|
-	if bad_words.include?(w) then
-		c.push(w)
-	else
-		c.push(w.capitalize!)
-		end
-	end
-	c[0].capitalize!
-	c.join(" ")
+  bad_words = %w(and over the in of)
+  c = []
+  s.split(" ").each do |w|
+    bad_words.include?(w) ? c.push(w) : c.push(w.capitalize!)
+  end
+  c[0].capitalize!
+  c.join(' ')
 end
